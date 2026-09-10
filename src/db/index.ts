e9d5a44acb5327d61175a8e824924403ed620e85
igonurcahyo <igonur322@gmail.com>
2026-09-10 14:56:07 +0700

    chore: diagnose production database connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,15 @@
 import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 
-const client = postgres(process.env.DATABASE_URL!)
+const databaseUrl = process.env.DATABASE_URL
+
+console.log(
+    '[DB] DATABASE_URL:',
+    databaseUrl
+        ? `${databaseUrl.split('@')[0]}@${databaseUrl.split('@')[1]?.split('/')[0]}`
+        : 'MISSING',
+)
+
+const client = postgres(databaseUrl!)
 
 export const db = drizzle(client)
